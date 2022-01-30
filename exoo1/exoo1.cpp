7ffa3541f3e966d36fc9b9a5f7e7fc9379f50c4b
Hadi006 <90691741+Hadi006@users.noreply.github.com>
2022-01-30 17:58:19 -0500

    Add files via upload
    push final

--- exoo1/exoo1.cpp
+++ exoo1/exoo1.cpp
@@ -9,10 +9,10 @@ using namespace std;
 
 int main()
 {
-    int nb;
-    cout << "Entrez un nombre : "; cin >> nb;
-    for (int i = 2; i <= sqrt(nb); ++i) {
-        if (nb % i == 0) {
+    int nombre;
+    cout << "Entrez un nombre : "; cin >> nombre;
+    for (int i = 2; i <= sqrt(nombre); ++i) {
+        if (nombre % i == 0) {
 
             cout << "ce nombre n'est pas premier car il est divisible par " << i << ".";
             return 0;
@@ -23,13 +23,4 @@ int main()
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
