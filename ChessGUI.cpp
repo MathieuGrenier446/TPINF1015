42a72c48a3bd1727fd24b05e8611488c943f551f
Hadi006 <90691741+Hadi006@users.noreply.github.com>
2022-04-10 23:38:19 -0400

    Update ChessGUI.cpp

--- ChessGUI.cpp
+++ ChessGUI.cpp
@@ -1,6 +1,7 @@
-//#include <QApplication>
-//#include <QPushButton>
-//
+#include <QApplication>
+#include <QPushButton>
+#include <QtGui>
+#include<Qwidget>
 //int main(int argc, char* argv[])
 //{
 //    QApplication app(argc, argv);
@@ -11,3 +12,40 @@
 //    return app.exec();
 //    
 //}
+
+
+
+class MyWidget : public QWidget
+{
+    public:
+    MyWidget();
+
+    protected:
+    void paintEvent(QPaintEvent*);
+};
+
+MyWidget::MyWidget()
+{
+    QPalette palette(MyWidget::palette());
+    palette.setColor(backgroundRole(), Qt::darkGray);
+    setPalette(palette);
+}
+
+void MyWidget::paintEvent(QPaintEvent*)
+{
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setPen(Qt::darkGreen);
+    painter.drawRect(2, 8, 6, 4);
+
+    painter.setPen(Qt::darkGray);
+    painter.drawLine(2, 8, 6, 2);
+}
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+    MyWidget widget;
+    widget.show();
+    return app.exec();
+}
